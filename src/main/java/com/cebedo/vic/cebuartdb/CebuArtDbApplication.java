23d027febaef2845e14ba23ef7b97b7138a34a2b
Victorio Cebedo II <cebedo.vii@gmail.com>
2019-09-10 16:53:14 +0800

    Initial commit.

---
+++ src/main/java/com/cebedo/vic/cebuartdb/CebuArtDbApplication.java
@@ -0,0 +1,13 @@
+package com.cebedo.vic.cebuartdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebuArtDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebuArtDbApplication.class, args);
+	}
+
+}
